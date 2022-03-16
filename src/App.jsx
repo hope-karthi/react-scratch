ef848e735b27f1d3ec994b00396abde6433a28b4
emp3.spacebuy <emp3.spacebuy@gmail.com>
2022-03-16 11:24:57 +0530

    react refresh in dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './style.css'
+import {Clickcounter} from './Clickcounter'
 export const App = () => {
   return (
     <div>A1pp
-      <h1>HELLO WORLD</h1>
+      <h1>HELLO 78784 WORLD</h1>
       {process.env.NODE_ENV}----{process.env.name}
+	<Clickcounter/>
     </div>
   )
 }
